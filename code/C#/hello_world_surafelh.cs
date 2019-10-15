a9683bcd66cfc892ccca0f0db4a149ba1140ade9
Surafel <surafel.hailu@gmail.com>
2019-10-15 11:36:34 +0300

    Added hello world class

---
+++ code/C#/hello_world_surafelh.cs
@@ -0,0 +1,16 @@
+//AUTHOR: surafel
+//LANGUAGE: C#
+//GITHUB: https://github.com/surafelh
+
+using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, from surafel");
+        }
+    }
+}
